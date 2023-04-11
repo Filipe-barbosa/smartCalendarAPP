7d52f64b3d9e08345ef24a78cc3b2998e7b88856
Raysson Ornelas <rayssonornelas2006@gmail.com>
2023-04-11 19:44:33 -0300

    enh:htmlFor remove

--- src/components/Inputs/index.tsx
+++ src/components/Inputs/index.tsx
@@ -22,12 +22,7 @@ type TimeInputProps = {
 export const TimeInput: React.FC<TimeInputProps> = ({ title }) => {
   return (
     <div>
-      <label
-        htmlFor="consultation-time"
-        className="block text-gray-700 font-bold mb-2"
-      >
-        {title}
-      </label>
+      <label className="block text-gray-700 font-bold mb-2">{title}</label>
       <input
         type="time"
         className="border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
