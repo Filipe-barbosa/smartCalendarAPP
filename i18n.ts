d1f18014abb05f74cb96f6383c3d5a5401218baa
Raysson Ornelas <rayssonornelas2006@gmail.com>
2023-05-18 18:38:06 -0300

    enh:comment removal

--- i18n.ts
+++ i18n.ts
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-// import LanguageDetector from 'i18next-browser-languagedetector'; Se quiser adicionar para pegar a linguagem do browser.
 
 import translations from './locales';
 
