9994f3f6a1b50bcd6f1c40ee3f6045c0fb7a1de7
Filipe-barbosa <filipebarbosa94@hotmail.com>
2023-05-30 22:06:14 -0300

    improvment: set close button position

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -3,20 +3,20 @@ type ModalForm = {
   onClose: () => void,
 };
 
-export const Modal: React.FC<ModalForm> = ({ children }) => {
+export const Modal: React.FC<ModalForm> = ({ children, onClose }) => {
   return (
-    <div className="fixed z-50 inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center">
-      <div className="px-10 py-10 bg-white rounded-xl">
-        <div className="flex justify-between items-center">
-          <button
-            className="text-gray-600 hover:text-gray-900 transition-colors"
-            onClick={() => onclose}
-          >
-            X
-          </button>
-          {children}
-        </div>
+    <div className="fixed z-50 inset-0 flex items-center justify-center">
+      <div className="fixed inset-0 bg-gray-900 bg-opacity-50"></div>
+      <div className="relative max-w-3xl w-full mx-auto mt-10 bg-white rounded-xl">
+        <button
+          className="absolute top-0 right-0 m-4 text-gray-600 hover:text-gray-900 transition-colors"
+          onClick={onClose}
+        >
+        X
+        </button>
+        <div className="p-10">{children}</div>
       </div>
     </div>
+
   );
 };
